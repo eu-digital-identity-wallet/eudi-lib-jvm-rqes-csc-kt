6045c77fd98afb78bfe63de2f37b329da0278799
Giorgos Dimtsas <gedim21@gmail.com>
2025-06-30 10:49:40 +0300

    Add PadEs-B-T support (#48)
    * Implement TSA URL handling and timestamping in PodofoManager; add TimestampService for timestamp requests
    
    * Replace bundled podofo-android.aar with maven dependency
    
    * Formatting
    
    ---------
    
    Co-authored-by: hectorgat <hgat@netcompany.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,12 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        maven {
+            name = "Central Portal Snapshots"
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+        }
         mavenCentral()
+        mavenLocal()
         flatDir {
             dirs("libs")
         }
